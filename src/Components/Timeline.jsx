cd327e703d3e251f5963003ed334871fcab3a490
SanjayNithish <sanjay24112003@gmail.com>
2023-09-10 18:57:03 +0530

    Responsive

--- src/Components/Timeline.jsx
+++ src/Components/Timeline.jsx
@@ -13,63 +13,45 @@ const timeline = () => {
           <div className="timeline_content">
             <ul>
               <li>
-                <h3 className="title1">Design</h3>
-                <p>
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint
-                  veniam voluptas alia
-                </p>
+                <h3 className="title1">Registration Begins</h3>
+                <p>Let the fun begin</p>
                 <span className="circle"></span>
-                <span className="date">September 1</span>
+                <span className="date">26th January</span>
               </li>
 
               <li>
-                <h3 className="title1">wireframe</h3>
-                <p>
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint
-                  veniam voluptas alia
-                </p>
+                <h3 className="title1">Registration Closes</h3>
+                <p>Registration form closes</p>
                 <span className="circle"></span>
-                <span className="date">September 1</span>
+                <span className="date">7th February</span>
               </li>
 
               <li>
-                <h3 className="title1">coading</h3>
-                <p>
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint
-                  veniam voluptas alia
-                </p>
+                <h3 className="title1">Mentor Allocation</h3>
+                <p>You get your own mentor to clear all your doubts</p>
                 <span className="circle"></span>
-                <span className="date">September 1</span>
+                <span className="date">9th February</span>
               </li>
 
               <li>
-                <h3 className="title1">Testing</h3>
-                <p>
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint
-                  veniam voluptas alia
-                </p>
+                <h3 className="title1">Opening Ceremony</h3>
+                <p>Hackathon Begins</p>
                 <span className="circle"></span>
-                <span className="date">September 1</span>
+                <span className="date">10th February</span>
               </li>
 
               <li>
-                <h3 className="title1">Responsive</h3>
-                <p>
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint
-                  veniam voluptas alia
-                </p>
+                <h3 className="title1">Hackathon Concludes</h3>
+                <p>36 Hours of fun</p>
                 <span className="circle"></span>
-                <span className="date">September 1</span>
+                <span className="date">12th February</span>
               </li>
 
               <li>
-                <h3 className="title1">Deploy</h3>
-                <p>
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint
-                  veniam voluptas alia
-                </p>
+                <h3 className="title1">Results Announcement</h3>
+                <p>Happy Valentine's Day! 💖</p>
                 <span className="circle"></span>
-                <span className="date">September 1</span>
+                <span className="date">14th February</span>
               </li>
             </ul>
           </div>
